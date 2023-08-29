ceb80becf7b3382428ed24361295c9a4e0d6b6e6
alexramirez31 <alexramirez31@gmail.com>
2023-08-29 07:40:47 -0600

    login y control de rutas

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,22 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
 
-  constructor(private router: Router) { }
+  public loginForm= this.fb.group({
+    email: [localStorage.getItem('email') || '',[Validators.required, Validators.email]],
+    password: ['',[Validators.required]],
+    remember: [false]
+  });
 
-  ngOnInit(): void {
+  constructor(private router: Router , private fb:FormBuilder,private usuarioSvc:UsuarioService) { }
+
+  login(){
+    console.log(this.loginForm.value);
+    this.usuarioSvc.login(this.loginForm.value).subscribe(res =>{
+     if (this.loginForm.get('remember').value){
+        localStorage.setItem('email',this.loginForm.get('email').value);
+     }else{
+        localStorage.removeItem('email');
+     }
+
+     this.router.navigateByUrl('/dashboard');
+    
+    },(err)=>{
+      Swal.fire('Error',err.error.message,'error');
+    });
+
   }
 
-  onLogin() {
 
-    this.router.navigate(['']);
-
-  }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterForm } from '../interfaces/register-form.interface';
+import { LoginForm } from '../interfaces/login-form-iterface';
 import { environment } from 'src/environments/environment';
+import { tap } from 'rxjs/operators'
 
 //http://localhost:3000
 const URL = environment.urlServer;
@@ -16,6 +18,12 @@ export class UsuarioService {
 newUsuario(formData:RegisterForm){
   return this.http.post(`${URL}/usuarios`,formData,{responseType: 'text'});
 
+}
+
+login(formData:LoginForm){
+  return this.http.post(`${URL}/auth/login`,formData).pipe(tap((res:any)=>{
+    localStorage.setItem('token',res.token);
+  }));
 }
 
 }
